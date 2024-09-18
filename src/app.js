9a38b3ab84988c08409721e245a10bb8d52e09be
Vineet TIWARI <vineet.tiwari.ext@alten.com>
2024-09-18 17:47:01 +0530

    middleware simple example

--- src/app.js
+++ src/app.js
@@ -2,12 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Hello World")
+app.use("/admin", (req, res, next) => {
+    console.log("Admin auth is getting checked!")
+    const token = "xyz"
+    const isAdminAuthorized = token === "xyz";
+    if (isAdminAuthorized) {
+        next()
+    } else {
+        res.send("Admin is not authorized")
+    }
 })
+
+app.use("/admin/getAllData", (req, res) => {
+    res.send("Get All Admin Data")
+})
+
+app.use("/admin/deleteUser", (req, res) => {
+    res.send("Delete User")
+})
+
 
 app.use("/test", (req, res) => {
     res.send("Hello Test")
+})
+
+
+app.use("/", (req, res) => {
+    res.send("Hello World")
 })
 
 app.listen(3000, () => {
